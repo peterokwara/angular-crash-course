7c249c348e68f8e6747b8f23520a07e342ebf66f
Peter Okwara <peter.okwara@outlook.com>
2019-10-21 18:37:00 +0300

    Remove todo information from the component into a service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from "../../models/Todo";
+import { TodoService } from "../../services/todo.service"
 
 @Component({
   selector: 'app-todos',
@@ -10,34 +11,10 @@ export class TodosComponent implements OnInit {
 
   todos: Todo[]
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: false
-
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: true
-
-      }, {
-        id: 4,
-        title: 'Todo Four',
-        completed: false
-
-      }
-    ]
+    this.todos = this.todoService.getTodos();
   }
 
 }
